a114909eb5e59aed05fcd1ec89187467d4435caa
0xsid0703 <daniel73matthew1213@gmail.com>
2024-12-15 09:24:08 -0500

    added backend api

--- redux/apiConfig.ts
+++ redux/apiConfig.ts
@@ -1,3 +1,4 @@
 // redux/apiConfig.ts
-export const apiUrl = process.env.NEXT_API_URL;
+export const apiUrl =
+  process.env.NEXT_API_URL || "https://8a41-95-164-119-121.ngrok-free.app/";
 // export const apiUrl = process.env.NEXT_API_URL || "http://localhost:5000";
