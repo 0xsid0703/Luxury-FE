04a2ef7b769e0995bf2a82899b89df5f2c4b800a
0xsid0703 <daniel73matthew1213@gmail.com>
2024-12-15 09:21:14 -0500

    changed email

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,17 +4,17 @@ import "./globals.css";
 import SideBar from "@/components/SideBar";
 import { Toaster } from "@/components/ui/toaster";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
 const geistMono = localFont({
   src: "./fonts/GeistMonoVF.woff",
   variable: "--font-geist-mono",
   weight: "100 900",
 });
 
+const geistSans = localFont({
+  src: "./fonts/GeistVF.woff",
+  variable: "--font-geist-sans",
+  weight: "100 900",
+});
 export const metadata: Metadata = {
   title: "Transition Today",
   description: "Created by 0xsid0703",
