70ec01a47b5032a85507aa9b796137a45b872a04
0xsid0703 <daniel73matthew1213@gmail.com>
2024-12-19 07:30:02 -0500

    updated

--- next.config.mjs
+++ next.config.mjs
@@ -8,6 +8,12 @@ const nextConfig = {
           port: '5000', // Specify the port if the backend runs on a custom port
           pathname: '/images/**', // Match your image path
         },
+        {
+          protocol: 'http',
+          hostname: '95.164.119.121',
+          port: '5000', // Specify the port if the backend runs on a custom port
+          pathname: '/images/**', // Match your image path
+        }
       ],
     },
   };
