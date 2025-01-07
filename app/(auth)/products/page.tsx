45b406a8962cb8baab1fd2dc534cfffa46417803
0xsid0703 <daniel73matthew1213@gmail.com>
2025-01-07 11:30:26 -0500

    updated trigger

--- app/(auth)/products/page.tsx
+++ app/(auth)/products/page.tsx
@@ -6,34 +6,13 @@ import ProductEdition from "@/components/products/ProductEdition";
 import ProductFooter from "@/components/products/ProductFooter";
 import ProductHero from "@/components/products/ProductHero";
 import ProductMarket from "@/components/products/ProductMarket";
-import React, { useState } from "react";
+import React from "react";
 
 const ProductPage = () => {
-  const [scriptLoaded, setScriptLoaded] = useState(false);
-
   const loadMailchimpPopup = () => {
-    if (!scriptLoaded) {
-      const script = document.createElement("script");
-      script.id = "mcjs";
-      script.src =
-        "https://chimpstatic.com/mcjs-connected/js/users/9ae7f4c2cd8fb05a3073a6f81/418afc31df317a39db97f2028.js";
-      script.async = true;
-      document.body.appendChild(script);
-
-      script.onload = () => {
-        console.log("Mailchimp popup script loaded.");
-        setScriptLoaded(true);
-        // Now that the script is loaded, trigger the popup
-        if (window.mc4wp) {
-          window.mc4wp.openPopup(); // Show Mailchimp popup
-        }
-      };
-    } else {
-      // If the script is already loaded, just show the popup
       if (window.mc4wp) {
         window.mc4wp.openPopup();
       }
-    }
   };
   
   return (
