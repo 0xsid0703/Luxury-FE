95903f885e8cd0226225a76854dcb2ffab7c4726
0xsid0703 <daniel73matthew1213@gmail.com>
2025-01-07 12:44:25 -0500

    change head

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,21 +1,23 @@
-import Head from "next/head";
-import React from "react";
+import React, { useEffect } from "react";
 
 const layout = ({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) => {
-  return <>
-    <Head>
-      <script
-        id="mcjs"
-        async
-        src="https://chimpstatic.com/mcjs-connected/js/users/9ae7f4c2cd8fb05a3073a6f81/418afc31df317a39db97f2028.js"
-      ></script>
-    </Head>
-    <div className="w-full bg-mainBackground h-screen">{children}</div>
-  </>;
+  useEffect(() => {
+    const script = document.createElement("script");
+    script.id = "mcjs";
+    script.src =
+      "https://chimpstatic.com/mcjs-connected/js/users/9ae7f4c2cd8fb05a3073a6f81/418afc31df317a39db97f2028.js";
+    script.async = true;
+    document.head.appendChild(script);
+
+    return () => {
+      document.head.removeChild(script); // Clean up if the component unmounts
+    };
+  }, []);
+  return <div className="w-full bg-mainBackground h-screen">{children}</div>;
 };
 
 export default layout;
