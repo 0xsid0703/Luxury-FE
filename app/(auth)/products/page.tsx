8c9e00b8dc6d9fcc5d2480b323c4f67901892f1c
0xsid0703 <daniel73matthew1213@gmail.com>
2025-01-07 13:33:40 -0500

    change head

--- app/(auth)/products/page.tsx
+++ app/(auth)/products/page.tsx
@@ -15,7 +15,7 @@ const ProductPage = () => {
       // eslint-disable-next-line @typescript-eslint/no-explicit-any
       window.dojoRequire(["mojo/signup-forms/Loader"], function (L: any) {
         L.start({
-          baseUrl: "mc.us1.list-manage.com",
+          baseUrl: "mc.us12.list-manage.com",
           uuid: "9ae7f4c2cd8fb05a3073a6f81",
           lid: "418afc31df317a39db97f2028",
         });
