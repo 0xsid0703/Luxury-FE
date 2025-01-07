63621985f3d18de7a38a281010974e0188bc4092
0xsid0703 <daniel73matthew1213@gmail.com>
2025-01-07 12:52:31 -0500

    change head

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,23 +1,10 @@
-"use client";
-import React, { useEffect } from "react";
+import React from "react";
 
 const layout = ({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) => {
-  useEffect(() => {
-    const script = document.createElement("script");
-    script.id = "mcjs";
-    script.src =
-      "https://chimpstatic.com/mcjs-connected/js/users/9ae7f4c2cd8fb05a3073a6f81/418afc31df317a39db97f2028.js";
-    script.async = true;
-    document.head.appendChild(script);
-
-    return () => {
-      document.head.removeChild(script); // Clean up if the component unmounts
-    };
-  }, []);
   return <div className="w-full bg-mainBackground h-screen">{children}</div>;
 };
 
