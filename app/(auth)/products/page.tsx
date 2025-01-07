7a6b09ea19488fcdc9a23f79ecc7403b42a6aad1
0xsid0703 <daniel73matthew1213@gmail.com>
2025-01-07 10:55:10 -0500

    added pop up

--- app/(auth)/products/page.tsx
+++ app/(auth)/products/page.tsx
@@ -5,11 +5,25 @@ import ProductEdition from "@/components/products/ProductEdition";
 import ProductFooter from "@/components/products/ProductFooter";
 import ProductHero from "@/components/products/ProductHero";
 import ProductMarket from "@/components/products/ProductMarket";
-import React from "react";
+import Head from "next/head";
+import React, { useEffect } from "react";
 
 const ProductPage = () => {
+  useEffect(() => {
+    // Ensure the script is loaded
+    const script = document.createElement("script");
+    script.src =
+      "https://chimpstatic.com/mcjs-connected/js/users/9ae7f4c2cd8fb05a3073a6f81/418afc31df317a39db97f2028.js";
+    script.async = true;
+    document.body.appendChild(script);
+  }, []);
   return (
     <div className="min-h-screen">
+      <Head>
+        {/* Add Mailchimp metadata */}
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta charSet="utf-8" />
+      </Head>
       <ProductHero />
       <ProductEdition />
       <ProductBrand />
