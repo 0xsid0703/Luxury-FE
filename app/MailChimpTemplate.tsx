3f0653fb899ccd498260862c8eec3ddfff1e533e
0xsid0703 <daniel73matthew1213@gmail.com>
2025-01-07 20:21:08 -0500

    fix

--- app/MailChimpTemplate.tsx
+++ app/MailChimpTemplate.tsx
@@ -5,27 +5,7 @@ import { useEffect } from "react";
 
 const MailchimpPopupTemplate = () => {
   useEffect(() => {
-    // Ensure the script only loads once, on the first render
-    const loadMailchimpScript = () => {
-      if (!window._mailchimpLoaded) {
-        window._mailchimpLoaded = true; // Prevent the script from loading again
-
-        const script = document.createElement("script");
-        script.id = "mcjs";
-        script.src = "https://chimpstatic.com/mcjs-connected/js/users/9ae7f4c2cd8fb05a3073a6f81/418afc31df317a39db97f2028.js";
-        script.async = true;
-        script.defer = true;
-        document.head.appendChild(script);
-
-        script.onload = () => {
-          console.log("Mailchimp script loaded");
-        };
-      }
-    };
-
-    // Disable auto-open and load the script only once
-    loadMailchimpScript();
-    
+    // Function to disable auto-opening of the popup and initialize it
     const preventMailchimpAutoPopup = () => {
       let originalDojoRequire = window.dojoRequire;
 
@@ -34,29 +14,38 @@ const MailchimpPopupTemplate = () => {
           return originalDojoRequire;
         },
         set(value) {
+            console.log({value}, window._mcPrevented)
           if (value && !window._mcPrevented) {
             window._mcPrevented = true;
             // eslint-disable-next-line @typescript-eslint/no-explicit-any
             value(["mojo/signup-forms/Loader"], function (L: any) {
-              const originalStart = L.start;
-              // eslint-disable-next-line @typescript-eslint/no-explicit-any
-              L.start = function (config: any) {
-                // Disable auto-open for the popup
-                config.autoOpenDisabled = true;
-                originalStart.call(this, config);
-              };
+                const originalStart = L.start;
+                // eslint-disable-next-line @typescript-eslint/no-explicit-any
+                L.start = function (config: any) {
+                    // Disable auto open
+                    config.autoOpenDisabled = true;
+                    originalStart.call(this, config);
+                };
             });
-          }
-          originalDojoRequire = value;
-        },
-      });
+        }
+        originalDojoRequire = value;
+    },
+});
     };
 
     preventMailchimpAutoPopup();
-  }, []);
+}, []);
 
-  return null;
+return (
+    <head>
+      <script
+        id="mcjs"
+        async
+        defer
+        src="https://chimpstatic.com/mcjs-connected/js/users/9ae7f4c2cd8fb05a3073a6f81/418afc31df317a39db97f2028.js"
+        ></script>
+    </head>
+  );
 };
 
 export default MailchimpPopupTemplate;
-
