4521986bbbc9b22af69594b32450b7fa6cfff420
0xsid0703 <daniel73matthew1213@gmail.com>
2025-01-22 09:35:12 -0500

    added login logic

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,11 +1,19 @@
+import { getCurrentUser } from "@/lib/auth";
+import { redirect } from "next/navigation";
 import React from "react";
 
-const layout = ({
+const layout = async({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) => {
-  return <div className="w-full bg-mainBackground h-screen">{children}</div>;
+  const user = await getCurrentUser();
+  if (!user) {
+    redirect('/login')
+  }
+  return (
+    <div className="w-full bg-mainBackground h-screen">{children}</div>
+  );
 };
 
 export default layout;
--- app/(auth)/products/[collection]/page.tsx
+++ app/(auth)/products/[collection]/page.tsx
@@ -1,10 +1,14 @@
 import ProductPage from "@/components/products/ProductMain";
 import { getCollectionProducts } from "@/lib/shopify";
+import { redirect } from "next/navigation";
 import React from "react";
 
 export default async function Page(props: { params: Promise<{ collection: string }> }) {
   const { collection } = await props.params;
   const products = await getCollectionProducts({ collection });
+  if (!collection) {
+    redirect(`/`);
+  }
   console.log({ products })
 
   return <div><ProductPage products={products} /></div>;
