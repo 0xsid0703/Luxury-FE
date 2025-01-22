fe36ef964548748756029696b390b7f42427507b
0xsid0703 <daniel73matthew1213@gmail.com>
2025-01-22 08:50:36 -0500

    added auth

---
+++ app/(auth)/template.tsx
@@ -0,0 +1,21 @@
+"use client";
+import { getCurrentUser } from "@/lib/auth";
+import { redirect } from "next/navigation";
+import React, { PropsWithChildren } from "react";
+
+const RootTemplate = ({ children }: PropsWithChildren) => {
+    return <>
+        <MainComponent />
+        {children}
+
+    </>;
+};
+
+export default RootTemplate;
+const MainComponent = async() => {
+    const user = await getCurrentUser();
+    if(!user) {
+        redirect("/login");
+    }
+    return <></>;
+};
